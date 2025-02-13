6156a4d77bb0f875caeeb7cb430b588152f23507
Koki Onodera <onodera.pbl@gmail.com>
2025-02-13 23:32:59 +0900

    modify

--- src/test/java/BooksNormalPurchase.java
+++ src/test/java/BooksNormalPurchase.java
@@ -1,8 +1,11 @@
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import com.thoughtworks.gauge.AfterSuite;
 import com.thoughtworks.gauge.BeforeSuite;
 import com.thoughtworks.gauge.Step;
 import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -12,7 +15,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import util.TestUtils;
 
 public class BooksNormalPurchase{
-    static String BASKET_URL = "https://books.step.rakuten.co.jp/rms/mall/book/bs/Cart?shop_bid=213310&item_id=10269286&units=1";
+    static String BASKET_URL = "https://books.step.rakuten.co.jp/rms/mall/book/bs/Cart?shop_bid=213310&item_id=20971294&units=1";
     static String capturePath;
     static WebDriver webDriver;
 
@@ -39,19 +42,25 @@ public class BooksNormalPurchase{
         element.click();
     }
     @Step("<userId><password>を入力し、「次へ」を押下し、Step4に遷移する")
-    public void login(String userId, String password) {
-        // googleの検索ボックスはName要素にqが設定されている
-        WebElement idForm = webDriver.findElement(By.name("u"));
-        WebElement passForm = webDriver.findElement(By.name("p"));
-
-
+    public void inputUserId(String userId, String pass) {
+        //要素を見つけるまで待機
+        webDriver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        WebElement idForm = webDriver.findElement(By.name("username"));
         idForm.sendKeys(userId);
-        passForm.sendKeys(password);
+        idForm.sendKeys(Keys.chord(Keys.ENTER));
+        webDriver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        WebElement passForm = webDriver.findElement(By.name("password"));
+        passForm.sendKeys(pass);
         passForm.sendKeys(Keys.chord(Keys.ENTER));
     }
 
     @Step("キャプチャを取得")
-    public void capture() throws IOException{
+    public void capture() throws IOException, InterruptedException {
+        // 画面表示まで１秒待つ
+        // TODO　将来的には画面表示されるまで待つように修正する
+        long millSec = 1000;
+        Thread.sleep(millSec);
+
         // 画面をキャプチャする
         TestUtils.screenShot(webDriver);
     }
