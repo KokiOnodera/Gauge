fec3f9106dc439669d6b9bbff7b0cfa8aee035f8
Koki Onodera <onodera.pbl@gmail.com>
2025-02-25 22:42:06 +0900

    modify

--- src/test/java/util/TestUtils.java
+++ src/test/java/util/TestUtils.java
@@ -22,7 +22,7 @@ public class TestUtils {
      * @throws IOException
      */
     public static void screenShot(WebDriver driver) throws IOException{
-        String dowloadsPath = System.getProperty("user.home");
+        String dowloadsPath = System.getProperty("user.home") + "\\AppData\\Roaming\\gauge\\logs\\capture";
         LocalDateTime nowDate = LocalDateTime.now();
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMddHHmm");
         String filename = "gauge-test-" + dtf.format(nowDate);
